bdb3f3d1234871cdbeeff42a2b69d155637815bb
Nikas Zabukas <nikas.jxd@gmail.com>
2025-12-10 03:45:31 +0200

    Fixed a spelling mistake that was quite crucial

--- backend/Server/Program.cs
+++ backend/Server/Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddScoped<IOrdersService, OrdersService>();
 builder.Services.AddScoped<IPaymentsService, PaymentsService>();
 builder.Services.AddScoped<IReceiptsService, ReceiptsService>();
 builder.Services.AddScoped<IServicesService, ServicesService>();
-builder.Services.AddScoped<VatService, VatService>();
+builder.Services.AddScoped<IVatService, VatService>();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
