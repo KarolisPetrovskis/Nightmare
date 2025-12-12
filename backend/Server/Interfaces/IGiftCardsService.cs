894985a8e63dc47f52f08656694c86e6e2b5d3ca
Aleksej Krasavcev <aleksejkrasavcev@gmail.com>
2025-12-12 20:43:39 +0200

    GiftCards controller and service done; added some tests

--- backend/Server/Interfaces/IGiftCardsService.cs
+++ backend/Server/Interfaces/IGiftCardsService.cs
@@ -1,7 +1,12 @@
+using backend.Server.Models.DatabaseObjects;
+
 namespace backend.Server.Interfaces
 {
     public interface IGiftCardsService
     {
-        void placeholderMethod();
+        Task<List<GiftCard>> GetAllGiftCardsAsync(long businessId, int page, int perPage);
+        Task CreateGiftCardAsync(GiftCard giftCard);
+        Task<GiftCard> GetGiftCardByNidAsync(long nid);
+        Task DeleteGiftCardAsync(long nid);
     }
 }
--- backend/Server/Services/GiftCardsService.cs
+++ backend/Server/Services/GiftCardsService.cs
@@ -1,12 +1,74 @@
+using backend.Server._helpers;
+using backend.Server.Database;
+using backend.Server.Exceptions;
 using backend.Server.Interfaces;
+using backend.Server.Models.DatabaseObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Server.Services
 {
-    public class GiftCardsService : IGiftCardsService
+    public class GiftCardsService(ApplicationDbContext context) : IGiftCardsService
     {
-        public void placeholderMethod()
+        private readonly ApplicationDbContext _context = context;
+        public async Task<List<GiftCard>> GetAllGiftCardsAsync(long businessId, int page, int perPage)
         {
-            // Implementation of the placeholder method
+            if (page < 0)
+            {
+                throw new ApiException(400, "Page number must be greater than zero");
+            }
+            if (perPage <= 0)
+            {
+                throw new ApiException(400, "PerPage value must be greater than zero");
+            }
+
+            if (page == 0)
+            {
+                return await _context.GiftCard
+                    .Where(g => g.BusinessId == businessId)
+                    .AsNoTracking()
+                    .ToListAsync();
+            }
+
+            return await _context.GiftCard
+                .Where(g => g.BusinessId == businessId)
+                .Skip((page - 1) * perPage)
+                .Take(perPage)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task CreateGiftCardAsync(GiftCard giftCard)
+        {
+            if (await _context.GiftCard.AnyAsync(g => g.Code == giftCard.Code && g.BusinessId == giftCard.BusinessId))
+            {
+                throw new ApiException(409, $"Gift card with Code {giftCard.Code} already exists.");
+            }
+
+            _context.GiftCard.Add(giftCard);
+
+            await Helper.SaveChangesOrThrowAsync(_context, "Failed to create gift card.");
+        }
+
+        public async Task<GiftCard> GetGiftCardByNidAsync(long nid)
+        {
+            if (nid <= 0)
+            {
+                throw new ApiException(400, "Nid must be a positive number");
+            }
+
+            var giftCard = await _context.GiftCard
+                .AsNoTracking()
+                .FirstOrDefaultAsync(g => g.Nid == nid) ?? throw new ApiException(404, $"Gift card with Nid {nid} not found.");
+            return giftCard;
+        }
+
+        public async Task DeleteGiftCardAsync(long nid)
+        {
+            var giftCard = await _context.GiftCard.FirstOrDefaultAsync(g => g.Nid == nid) ?? throw new ApiException(404, $"Gift card with Nid {nid} not found.");
+            
+            _context.GiftCard.Remove(giftCard);
+
+            await Helper.SaveChangesOrThrowAsync(_context, "Failed to delete gift card.");
         }
     }
 }
