aa924e18e9a52d50fbffa85cbc86dd33a3117ee6
Nikas Zabukas <nikas.jxd@gmail.com>
2025-12-12 23:45:49 +0200

    Removed allitems as we wont use it anyway

--- backend/Server/Models/Helpers/AllItems.cs
+++
@@ -1,9 +0,0 @@
-    using backend.Server.Models.DatabaseObjects;
-
-    namespace backend.Server.Models.Helpers;
-    public class AllItems<Class>
-    {
-        public List<Class>? List { get; set; }
-        public int Page { get; set; }
-        public int PerPage { get; set; }
-    }
