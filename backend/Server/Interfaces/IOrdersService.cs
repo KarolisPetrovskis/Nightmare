825a202e2794ac6be99fb390f0ff16b456096698
Aleksej Krasavcev <144383208+AKrasavcev@users.noreply.github.com>
2025-12-14 15:04:44 +0200

    Order controller and service (#21)
    * [WIP] added order getAll, create with corresponding methods
    
    * finished OrdersContoller and service; created more endpoints than initially intended; added some tests in `backend.http` which can be run with 'REST Client' extention
    
    * Refactor OrdersController and OrdersService constructors for improved readability
    
    * moved emtity creation to service
    
    * Fixed `.FindAsync(nid)` and moved update creation to the service

--- backend/Server/Interfaces/IOrdersService.cs
+++ backend/Server/Interfaces/IOrdersService.cs
@@ -1,7 +1,22 @@
+using backend.Server.Models;
+using backend.Server.Models.DatabaseObjects;
+using backend.Server.Models.DTOs.Order;
+
 namespace backend.Server.Interfaces
 {
     public interface IOrdersService
     {
-        void placeholderMethod();
+        Task<List<Order>> GetAllOrdersAsync(int page, int perPage);
+        Task<List<Order>> TrimOrdersByWorkerIdAsync(List<Order> orders, long workerId);
+        Task<List<Order>> TrimOrdersByDateAsync(List<Order> orders, DateTime date);
+        Task<Order> CreateOrderAsync(OrderCreateDTO request);
+        Task<List<OrderDetail>> CreateOrderDetailsAsync(List<OrderDetailRequest> orderDetailsRequest, long orderNid);
+        Task CreateOrderDetailAddOnsAsync(List<OrderDetailAddOn> orderDetailAddOns);
+        Task<Order> GetOrderByNidAsync(long nid);
+        Task<List<OrderDetail>> GetOrderDetailsByOrderId(long orderNid);
+        Task<List<OrderDetailAddOn>> GetOrderDetailAddOnsByDetailId(long detailNid);
+        Task<List<Order>> GetOrderByBusinessIdAsync(long businessId);
+        Task UpdateOrderAsync(OrderUpdateDTO request, long nid);
+        Task DeleteOrderAsync(long nid);
     }
 }
