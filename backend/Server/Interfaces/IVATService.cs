f4244a9b9ad94e60fd7fefa053636b03da4352b7
NikasNikasNikas <nikas.jxd@gmail.com>
2025-12-14 15:15:20 +0200

    Vat controller and service (#18)
    * Added the missing links.
    
    * Implemented Vat services
    
    * Fixed a spelling mistake that was quite crucial
    
    * Fixed namespace misaligment.
    
    * Create a proper return for GetVatRateBYNid in Vat controller
    
    * Made it so instead of returning codes it throws exceptions.
    
    * Aesthetic changes
    
    * Fixed a typo
    
    * Fixed a messed up merge and made percentage nullable in Vat (so we can tell when updating when its no discount and when its an empty field (we dont want toupdate percentage))
    
    * Fixed the Get for all vats request to not use a non existant body, also fixed get all vats so it returns based on the page requirments.
    
    * Removed nid generation in code as it is done by the DB automatically. Removed a repeat import in program.cs
    
    * Some DTO data requirments got lost, returned them.
    
    * It would be better to set null instead of 0 so just added the ability to do that.
    
    * Maded it so in creation if 0 is passed it makes it into null, also made it so the dto is nolonger nullable (like it was before the last commit).
    
    * Remade it so the vatcreatedto can be null.
    
    * Made the controller calls async.
    
    * Made is so vat creation return would have refrence to it based off of the apointment controller creation.
    
    * Adressed issues mentioned in PR.
    
    * Refactor VAT naming conventions and improve VATService methods for consistency
    
    * Removed allitems as we wont use it anyway
    
    * Remove unused helper imports and update GetVatRatesAsync return type to List<Vat>
    
    * Add handling for zero page request in GetVatRates method
    
    * Refactor Vat class to enforce required Name property and streamline GetVatRates method in VATService
    
    * Improve error messages for VAT rate retrieval and update methods in VATService
    
    * Remove unnecessary semicolon in CreateVatRateAsync method in VATController
    
    * Dealing with the issues of merge conflicts.

--- backend/Server/Interfaces/IVATService.cs
+++ backend/Server/Interfaces/IVATService.cs
@@ -1,15 +1,14 @@
 using backend.Server.Models.DatabaseObjects;
 using backend.Server.Models.DTOs.VAT;
-using backend.Server.Models.Helpers;
 
 namespace backend.Server.Interfaces
 {
-    public interface IVatService
+    public interface IVATService
     {
-        Task<AllItems<Vat>> GetVatRates(VatGetAllDTO request);
+        Task<List<Vat>> GetVatRates(VatGetAllDTO request);
         Task<Vat> CreateVatRate(VatCreateDTO request);
         Task UpdateVatRate(VatUpdateDTO request, long nid);
-        Task<Vat?> GetVatRateByNid(long nid);
+        Task<Vat> GetVatRateByNid(long nid);
         Task DeleteVatRate(long nid);
     }
 }
